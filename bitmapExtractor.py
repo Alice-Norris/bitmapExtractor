cc940256c33c33e669808e194bbc98e049475da6
Alice-Norris <79813833+Alice-Norris@users.noreply.github.com>
2021-11-22 17:43:49 -0500

    Removed print lines, included cur file for testing.

--- bitmapExtractor.py
+++ bitmapExtractor.py
@@ -1,11 +1,10 @@
 import struct
-cursorDataReader =  open(file="eraser.cur", mode='rb+', buffering=0)
+cursorDataReader =  open(file="bucket.cur", mode='rb+', buffering=0)
 cursorData = bytes(cursorDataReader.read(6))
 
 byteDataParser = struct.Struct("<h")
 iconDirEntryParser = struct.Struct("<4b 2h 2i")
 headerData = [*byteDataParser.iter_unpack(cursorData)]
-print(headerData)
 for pos, val in enumerate(headerData):
     item = headerData.pop(pos)
     headerData.insert(pos, item[0])
@@ -15,7 +14,6 @@ if icoType == 1:
     icoType = '.ICO'
 if icoType == 2:
     icoType = '.CUR'
-print(headerData)
 
 imgCount = headerData[2]
 iconDirEntries = []
@@ -23,10 +21,8 @@ while len(iconDirEntries) < imgCount:
     iconDirEntry = ([*iconDirEntryParser.unpack(bytes(cursorDataReader.read(16)))])
     iconDirEntry.pop(3)
     iconDirEntries.append(iconDirEntry)
-print(iconDirEntries)
-print(cursorDataReader.tell())
 
 for imageNum, imageHeader in enumerate(iconDirEntries):
     print(imageHeader)
-    print("Image Number: \n\tWidth: {1}\n\tHeight: {2}\n\tColor Count: {3}\n\tPlanes: {4}\n\tBitCount: {5}\n\tImage Data Size In Bytes: {6}\n\tImage Data Offset: {7}"
-    .format(imageNum+1, *imageHeader))
+    print("Image Number: {0}\n\tFile Type: {8}\n\tWidth: {1}\n\tHeight: {2}\n\tColor Count: {3}\n\tPlanes: {4}\n\tBitCount: {5}\n\tImage Data Size In Bytes: {6}\n\tImage Data Offset: {7}"
+    .format(imageNum+1, *imageHeader, icoType))
